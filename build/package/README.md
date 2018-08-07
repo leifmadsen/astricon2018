30b35a8ec68fb2717c92c17279fffbfd3aac0259
Leif <lmadsen@redhat.com>
2018-08-07 17:38:49 -0400

    Add basic builder script for Asterisk container

---
+++ build/package/README.md
@@ -0,0 +1,6 @@
+# asterisk-builder
+
+This directory contains a basic build script that will leverage `buildah` to
+build an Asterisk container image of Asterisk 15 from Fedora 29 RPMs.
+
+(Thanks to Jared Smith [jsmith] for continuing to make these!)
---
+++ build/package/asterisk-builder.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# set default version and build if not passed via arguments
+version=${1:-15.5.0}
+build=${2:-1}
+
+# start building from a scratch container
+astcontainer=$(buildah from scratch)
+astmount=$(buildah mount $astcontainer)
+
+# NOTE: unfortunately release 29 fails to run during a scriptlet on the info package 
+dnf install --disablerepo=* --enablerepo=fedora --enablerepo=updates \
+    --assumeyes --installroot $astmount --releasever=27 \
+    bash coreutils \
+    --setopt install_weak_deps=false \
+    --setopt tsflags=nodocs
+
+dnf clean all --assumeyes --installroot $astmount --releasever=27
+
+# install Asterisk, pjsip, and HEP
+dnf install --disablerepo=* --enablerepo=fedora --enablerepo=updates \
+    --assumeyes --installroot $astmount --releasever=29 \
+    asterisk-$version asterisk-pjsip-$version asterisk-hep-$version \
+    --setopt install_weak_deps=false \
+    --setopt tsflags=nodocs
+
+dnf clean all --assumeyes --installroot $astmount --releasever=29
+
+# set metadata for the container
+buildah config --annotation "site.nfvpe.asterisk.leifmadsen=$(uname -n)" $astcontainer
+buildah config --cmd "/usr/sbin/asterisk -cvvvv" $astcontainer
+buildah config --created-by "Leif Madsen and Doug Smith" $astcontainer
+buildah config --author "leif at redhat dot com" --label name=asterisk-$version-$build $astcontainer
+
+# unmount and commit changes
+buildah unmount $astcontainer
+buildah commit $astcontainer asterisk
+
+# make this available to our local docker process
+buildah push asterisk docker-daemon:astricon2018/asterisk:$version-$build
